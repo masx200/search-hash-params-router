70acad09cabbfb12726461486d8f4f3fba3f6b6f
masx200 <masx200@qq.com>
2021-07-01 17:27:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-import { gethashparams } from "./gethashparams";
-import { getsearchparams } from "./getsearchparams";
-import { sethashparams } from "./sethashparams";
-import { setsearchparams } from "./setsearchparams";
 import { createHashRouter } from "./hashrouter.js";
 import { createSearchRouter } from "./searchrouter.js";
 
-export { sethashparams, setsearchparams, getsearchparams, gethashparams };
-export { watchparams, unwatchparams };
 export { createHashRouter, createSearchRouter };
