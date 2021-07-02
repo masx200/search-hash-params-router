c9a92583791dd29165165cf01136af2dba8300c4
masx200 <masx200@qq.com>
2021-07-02 13:52:28 +0800

    Update index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+//@ts-ignore
+export * from "./create-react-link.ts";
+//@ts-ignore
+export * from "./create-react-view.ts";
+//@ts-ignore
+export * from "./create-vue-link.ts";
+//@ts-ignore
+export * from "./create-vue-view.ts";
