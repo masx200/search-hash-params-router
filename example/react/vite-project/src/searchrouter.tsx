4ffb0be14693f48cdd62ba540679a25df578f87e
masx200 <masx200@qq.com>
2021-07-05 14:43:11 +0800

    Update searchrouter.tsx

--- example/react/vite-project/src/searchrouter.tsx
+++ example/react/vite-project/src/searchrouter.tsx
@@ -2,6 +2,7 @@ import { createSearchRouter, matchroute } from "../../../../dist/index";
 import { routes } from "./routes";
 const searchrouter = createSearchRouter();
 console.log(searchrouter);
+console.log(searchrouter.getparams());
 searchrouter.on("params", (p) => {
     console.log(p);
 
