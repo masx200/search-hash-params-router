92c3d3df4cc35a9055c2d199a62a546f3f7c1fa6
masx200 <masx200@qq.com>
2021-07-09 14:24:55 +0800

    修改配置文件

--- example/vue/my-vue-app/vite.config.ts
+++ example/vue/my-vue-app/vite.config.ts
@@ -3,5 +3,13 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        cssCodeSplit: false,
+        polyfillDynamicImport: true,
+        target: "es2015",
+        terserOptions: {
+            compress: { drop_console: true, drop_debugger: true },
+        },
+    },
     plugins: [vue()],
 });
