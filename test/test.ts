04b383eb220d2bd700652b3451dac48834798663
masx200 <34191203+masx200@users.noreply.github.com>
2021-09-01 22:35:04 +0800

    test

---
+++ test/test.ts
@@ -0,0 +1,14 @@
+import {createPathRouter}from '../dist/index'
+const pathrouter=createPathRouter()
+pathrouter.mount()
+console.log(pathrouter)
+pathrouter.on('params',console.log)
+console.log(pathrouter.getparams())
+console.log(pathrouter.gethref({d:"2",a:"1"}))
+pathrouter.setparams({e:"6",p:"9"})
+console.log(pathrouter.getparams())
+pathrouter.setparams({f:"6",n:"9"})
+console.log(pathrouter.getparams())
+setTimeout(()=>{
+    pathrouter.unmount()
+},1000)
