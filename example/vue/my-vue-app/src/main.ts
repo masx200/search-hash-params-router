a017dafcdb6b220b6298c8832a508c940e1d487d
masx200 <34191203+masx200@users.noreply.github.com>
2021-09-01 22:12:54 +0800

    Update main.ts

--- example/vue/my-vue-app/src/main.ts
+++ example/vue/my-vue-app/src/main.ts
@@ -6,10 +6,12 @@ import routertest from "./routertest.vue";
 const app = createApp(routertest);
 
 app.mount(document.querySelector("#app") || "#app");
-
+!(
+async()=>{
 if (process.env.NODE_ENV === "production") {
     //@ts-ignore
     const { registerSW } = await import("virtual:pwa-register");
 
     registerSW({});
 }
+})()
