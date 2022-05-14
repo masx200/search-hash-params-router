4f68aedd5576160753395e27ec0491aac1ce57bd
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-14 13:47:33 +0800

    测试

--- test/test.test.ts
+++ test/test.test.ts
@@ -3,22 +3,26 @@ import {
     deserializeParams,
     serializeParams,
 } from "../dist/index";
-const pathrouter = createPathRouter();
-pathrouter.mount();
-console.log(pathrouter);
-pathrouter.on("params", console.log);
-console.log(pathrouter.getparams());
-console.log(pathrouter.gethref({ d: "2", a: "1" }));
-pathrouter.setparams({ e: "6", p: "9" });
-console.log(pathrouter.getparams());
-pathrouter.setparams({ f: "6", n: "9" });
-console.log(pathrouter.getparams());
-setTimeout(() => {
-    pathrouter.unmount();
-}, 1000);
-pathrouter.setparams({});
-console.log(pathrouter.getparams());
-let string = serializeParams({ foo: "bar", baz: "12321" });
-console.log(string);
-let object = deserializeParams("YmF6PTEyMzIxJmZvbz1iYXI=");
-console.log(object);
+import { test } from "vitest";
+test('pathrouter', async () => {
+    const pathrouter = createPathRouter();
+    pathrouter.mount();
+    console.log(pathrouter);
+    pathrouter.on("params", console.log);
+    console.log(pathrouter.getparams());
+    console.log(pathrouter.gethref({ d: "2", a: "1" }));
+    pathrouter.setparams({ e: "6", p: "9" });
+    console.log(pathrouter.getparams());
+    pathrouter.setparams({ f: "6", n: "9" });
+    console.log(pathrouter.getparams());
+    setTimeout(() => {
+        pathrouter.unmount();
+    }, 1000);
+    pathrouter.setparams({});
+    console.log(pathrouter.getparams());
+    let string = serializeParams({ foo: "bar", baz: "12321" });
+    console.log(string);
+    let object = deserializeParams("YmF6PTEyMzIxJmZvbz1iYXI=");
+    console.log(object);
+
+})
